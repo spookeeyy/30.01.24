d4f62c51dc73c0439a14e193bff6fbab1e99e3ab
spookeeyy <152895833+spookeeyy@users.noreply.github.com>
2024-01-30 16:04:12 +0200

    Add files via upload
    wowwwww

---
+++ tervoitisalem.py
@@ -0,0 +1,23 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+def tervita():
+    tervitus = "Tere, " + nimi.get()
+    messagebox.showinfo(message=tervitus)
+    
+    
+raam = Tk()
+raam.title("Tervitaja")
+raam.geometry("300x300")
+
+silt = ttk.Label(raam, text="Nimi")
+silt.place(x=5, y=5,)
+
+nimi = ttk.Entry(raam)
+nimi.place(x=70, y=5, width=150)
+
+nupp = ttk.Button(raam, text="Tervita!", command=tervita)
+nupp.place(x=70, y=40, width=150)
+
+raam.mainloop()
